fbf9ff6de223988b085c301a77441e0db2db7227
Papa05 <97911370+Papa05@users.noreply.github.com>
2022-01-20 23:12:28 -0800

    uploading. newspaper.

--- Karel_Newspaper.py
+++ Karel_Newspaper.py
@@ -1,9 +1,9 @@
 #====================================================
 # Filename: Karel_Newspaper.py
 # 
-# Your name:
-# Who did you work with (if anyone)?:
-# Estimate for time spent (in hrs)?:
+# Your name:papa lynch
+# Who did you work with (if anyone)?: prof. calvin 
+# Estimate for time spent (in hrs)?: 4hr im still no good
 #====================================================
 
 # I've just laid out a basic starting function below. 
@@ -19,18 +19,31 @@ def main():
     """ Function to cause Karel to retrieve the newspaper. """
     # You can add your sequence of commands below here!
     # Remember you can use the defined helper functions!
+    move()
+    move()
+    move()
+    turn_left()
+    pick_beeper()
+   
+    
+    
+    
 
 
 def move_to_newspaper():
     """ Helping function to move Karel to the newspaper location. """
+     move()
 
 
 
 def pick_up_paper():
     """ Helping function to have Karel pick up the newspaper. """
-    # Yes, this will probably be VERY simple and short
+    # Yes, this will probably be VERY simple and mshort
+    pick_beeper()
 
 
 
 def return_to_start():
     """ Helping function to move Karel back to its starting position. """
+    move()
+    turn_left()
